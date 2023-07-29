42cee1d664c5c7fa3e72a121d561ee75ff1df94c
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2023-07-29 20:09:58 -0300

    remove config files

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterApplicationServices(builder.Configuration);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://*:{port}");
-
 var app = builder.Build();
 
 app.ConfigureMiddleware();
